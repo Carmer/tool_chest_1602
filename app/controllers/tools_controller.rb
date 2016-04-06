0cc606fcf95699492a63737b1b92df55b31f5c65
Andrew Carmer <carmerandrew@gmail.com>
2016-04-05 22:42:44 -0600

    add show and new actions in tools controller

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -3,4 +3,12 @@ class ToolsController < ApplicationController
   def index
     @tools = Tool.all
   end
+
+  def show
+    @tool = Tool.find(params[:id])
+  end
+
+  def new
+    @tool = Tool.new
+  end
 end
