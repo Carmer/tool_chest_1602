61ab7c4fe216394b02e1e2b82741520621903b17
Andrew Carmer <carmerandrew@gmail.com>
2016-04-05 22:43:51 -0600

    add new.html.erb view and create in controller

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -11,4 +11,19 @@ class ToolsController < ApplicationController
   def new
     @tool = Tool.new
   end
+
+  def create
+    @tool = Tool.new(tool_params)
+    if @tool.save
+      redirect_to tool_path(@tool)   # Rails is 'smart' enough to also do => 'redirect_to @tool'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :price, :quantity)
+  end
 end
