a7729cf7076f7289a6d0645c0287a3cf4d208100
Andrew Carmer <carmerandrew@gmail.com>
2016-04-07 10:52:53 -0600

    add feature testing

---
+++ app/controllers/tools_controller.rb
@@ -0,0 +1,35 @@
+class ToolsController < ApplicationController
+
+  def index
+    @tools = Tool.all
+  end
+
+  def show
+    @tool = Tool.find(params[:id])
+  end
+
+  def destroy
+    tool = Tool.find( params[:id] )
+    tool.destroy
+    redirect_to tools_path
+  end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    tool = Tool.new( tool_params )
+    if tool.save
+      redirect_to tool_path(tool.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tool_params
+    params.require(:tool).permit(:name, :price, :quantity)
+  end
+end
